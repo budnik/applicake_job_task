802e7cc575bdc39dc6923dd6a319e65f6cceb20b
Dmitriy Budnik <dmitriy.budnik@gmail.com>
2012-09-24 11:04:20 +0300

    Functional style refactoring

--- lib/applicake.rb
+++ lib/applicake.rb
@@ -1,13 +1,9 @@
 class Applicake
   def decode(code)
-    code.split(',').inject([0]) do |answer, char|
-      if char.to_i > 0
-        answer[0] += char.to_i # count position
-      else
-        answer[answer[0]] = char # insert char in position
-        answer[0] = 0 # reset position counter
-      end
-      answer
-    end[1..-1].join
+    code.split(?,).reverse.slice_before{
+      |char| /[A-z ]/=~char
+    }.map{ 
+      |a| [a.shift, a.map(&:to_i).reduce :+].reverse 
+    }.sort.map(&:last).join
   end
 end
